8c870c963d3d4dc830bd64692c18705d4d76890f
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-09 21:58:09 +0100

    iteration 4 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,36 +1,46 @@
 const express = require('express');
 const celebrityRouter = express.Router();
-
 const Celebrity = require('./../models/celebrity');
 
 //
 celebrityRouter.get('/', (req, res, next) => {
   Celebrity.find()
-    .then(document => {
-      if(!document) {
+    .then((document) => {
+      if (!document) {
         const error = new Error();
         return Promise.reject(error);
       } else {
-        res.render('celebrities/index', {document})
+        res.render('celebrities/index', { document });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       next(error);
     });
 });
 
+celebrityRouter.get('/create', (req, res) => {
+  res.render('celebrities/create');
+});
+
+celebrityRouter.post('/create', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+
+  const newCelebrity = new Celebrity({ name, occupation, catchPhrase });
+  newCelebrity
+    .save()
+    .then((celebrity) => res.redirect('/'))
+    .catch((error) => res.render('celebrities/create'));
+});
+
 celebrityRouter.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  Celebrity.findOne( {_id: id})
-  .then(celebrityId => {
-      res.render('celebrities/show', {celebrityId})
-  })
-  .catch(error => {
-    next(error);
-  });
-})
-
-
-
+  Celebrity.findOne({ _id: id })
+    .then((celebrityId) => {
+      res.render('celebrities/show', { celebrityId });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 module.exports = celebrityRouter;
