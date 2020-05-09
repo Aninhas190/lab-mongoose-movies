4d5850fe8bcfccacf526e7e8bc52a43facf87473
Ana de Almeida <ana.isabel190@gmail.com>
2020-05-09 10:15:45 +0100

    iteration 3 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -3,25 +3,34 @@ const celebrityRouter = express.Router();
 
 const Celebrity = require('./../models/celebrity');
 
-
-// celebrityRouter.get('/', (req, res) => {
-//   res.render('/celebrities/index')
-// });
-
+//
 celebrityRouter.get('/', (req, res, next) => {
-  //res.send('abc');
   Celebrity.find()
-    .then(celebrity => {
-      if(!celebrity) {
+    .then(document => {
+      if(!document) {
         const error = new Error();
-        return Promise.reject(error)
+        return Promise.reject(error);
       } else {
-        res.render('celebrities/index', {celebrity})
+        res.render('celebrities/index', {document})
       }
     })
     .catch(error => {
       next(error);
     });
 });
+
+celebrityRouter.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findOne( {_id: id})
+  .then(celebrityId => {
+      res.render('celebrities/show', {celebrityId})
+  })
+  .catch(error => {
+    next(error);
+  });
+})
+
+
+
 
 module.exports = celebrityRouter;
